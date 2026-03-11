587a03c0dc1cf81d2b74ce4f1d685c729167a88c
Brendan Allan <git@brendonovich.dev>
2026-03-11 11:28:20 +0800

    cleanup

--- packages/opencode/src/lsp/server.ts
+++ packages/opencode/src/lsp/server.ts
@@ -1,4 +1,4 @@
-import { spawn as launch, type ChildProcessWithoutNullStreams, type SpawnOptionsWithoutStdio } from "child_process"
+import { spawn as launch, type ChildProcessWithoutNullStreams } from "child_process"
 import path from "path"
 import os from "os"
 import { Global } from "../global"
