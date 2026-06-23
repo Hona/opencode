d86e20ae16d3a267a61d25a775bc431050db29d9
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2026-06-23 18:01:17 +1000

    test(app): reproduce stale virtual range crash

--- packages/app/src/pages/session/timeline/virtual-items.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "bun:test"
-import { filterVirtualIndexes } from "./virtual-items"
-
-test("removes pinned indexes left behind after the timeline shrinks", () => {
-  expect(filterVirtualIndexes([0, 2, 4, 8], 5)).toEqual([0, 2, 4])
-})
