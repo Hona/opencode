8e85e4fac53ffb4a3d66f6903f6d5e12c9868653
LukeParkerDev <10430890+Hona@users.noreply.github.com>
2026-08-23 11:31:51 +1000

    Revert "fix(app): prevent clipped text descenders"
    This reverts commit e158e42199d75363de2ac8cffd43e1a511251883.

--- packages/app/src/shell/debug/debug-bar.tsx
+++ packages/app/src/shell/debug/debug-bar.tsx
@@ -88,9 +88,9 @@ function Cell(props: {
         </div>
         <div
           classList={{
-            "uppercase font-bold tabular-nums": true,
-            "text-[11px] leading-v2-tight": !!props.inline,
-            "text-[13px] leading-v2-compact sm:text-[14px]": !props.inline,
+            "uppercase leading-none font-bold tabular-nums": true,
+            "text-[11px]": !!props.inline,
+            "text-[13px] sm:text-[14px]": !props.inline,
             "text-text-on-critical-base": !!props.bad,
             "opacity-70": !!props.dim,
           }}
